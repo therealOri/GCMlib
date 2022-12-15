9a87bbd29d17603693a2b4f4febff4fff4e8427f
Ori <45724082+therealOri@users.noreply.github.com>
2022-12-15 11:46:52 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GCMlib",
+    packages=["gcm"],
+    version="1.0.0",
+    author="therealOri",
+    license="GPL-3.0",
+    install_requires=[
+        "alive-progress==2.4.1",
+        "pycryptodome==3.16.0",
+        "cryptography==38.0.4",
+    ],
+    author_email="therealOri@duck.com",
+    description="A minimalistic and simple encryption library. For encrypting data using AES GCM mode.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/therealOri/GCMlib",
+)
