254608664ee9c9d24975f4e4161ee4513cd86d7c
Ori <45724082+therealOri@users.noreply.github.com>
2023-01-17 15:08:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="GCMlib",
-    version="1.0.1",
+    version="1.0.2",
     author="therealOri",
     license="GPL-3.0",
     install_requires=[
